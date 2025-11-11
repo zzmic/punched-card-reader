e508ea4186faebcf7324013dd5051b97faccab5d
zzmic <100326374+zzmic@users.noreply.github.com>
2025-11-11 14:07:28 -0500

    Revise `docs/tlaplus-specification/cleanup.sh`

--- docs/tlaplus-specification/cleanup.sh
+++ docs/tlaplus-specification/cleanup.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 # `cleanup.sh`: clean up TLA+ execution artifacts other than source files ending in `.tla` and `.cfg`.
-echo 'WARNING: This script should only be executed from within the "tlaplus-specification" directory.'
-echo '         It will recursively delete all "states" directories and all ".out", ".dvi", ".tex", and ".pdf" files in the current directory.'
+echo 'WARNING: This script should only be executed from within the "tlaplus-specification" directory: '\
+'It will recursively delete all "states" directories and all ".out", ".dvi", ".tex", and ".pdf" files in the current directory.'
+
+# Prompt for user confirmation before proceeding.
+printf "Do you want to proceed with cleanup? (y/N): "
+read -r confirm
+case $confirm in
+    [yY])
+        echo "Proceeding with cleanup..."
+        ;;
+    *)
+        echo "Cleanup aborted."
+        exit 0
+        ;;
+esac
 
 # Remove all `states` directories in the current directory recursively.
 find . -type d -name states -prune -exec rm -r {} +
