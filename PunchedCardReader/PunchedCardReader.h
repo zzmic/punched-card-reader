d8810266fab96c5c9e8e97914a825235c3443574
AlexanderThaep <odomsathshia_tep@brown.edu>
2025-11-25 13:51:46 -0500

    Implement hardware test for IR LEDs

--- PunchedCardReader/PunchedCardReader.h
+++ PunchedCardReader/PunchedCardReader.h
@@ -3,16 +3,28 @@
 
 #include "Arduino.h"
 
+// These don't include the card detector pin
 #define READ_PINS_COUNT 6
 #define EMITTER_PINS_COUNT 12
 
 // P102
-const uint8_t READ_READY_PORT = 1;
-const uint8_t READ_READY_PIN = 2;
+const int READ_READY_PORT = 1;
+const int READ_READY_PIN = 2;
 
-const uint8_t READ_READY_EMITTER_PIN = D12;
+// Card detector pin
+const int READ_READY_EMITTER_PIN = D12;
 
-const uint8_t ANALOG_PINS[6] = { A0, A1, A2, A3, A4, A5 };
-const uint8_t DIGITAL_PINS[12] = { D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11 };
+// As defined by our latest pins schema
+const int ANALOG_PINS[6] = { A0, A1, A2, A3, A4, A5 };
+const int DIGITAL_PINS[12] = { D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11 };
+
+int readings_buffer[12];
+
+#define writePins(start_addr, end_addr, value) \
+  for (const int *pin = start_addr; pin != end_addr; pin++) digitalWrite(*pin, value);
+
+#define readPins(start_addr, end_addr, buffer) \
+  for (const int *pin = start_addr; pin != end_addr; pin++) \
+    { int *index = buffer; *index = analogRead(*pin); index++; }
 
 #endif
--- PunchedCardReader/PunchedCardReader.ino
+++ PunchedCardReader/PunchedCardReader.ino
@@ -1,31 +1,56 @@
 #include "Arduino.h"
 #include "PunchedCardReader.h"
 
-// the setup function runs once when you press reset or power the board
+void hardwareTest() {
+    Serial.println("Hardware test begin...");
+
+    writePins(ANALOG_PINS, ANALOG_PINS + EMITTER_PINS_COUNT / 2, HIGH);
+
+    readPins(ANALOG_PINS, ANALOG_PINS + EMITTER_PINS_COUNT / 2, readings_buffer);
+
+    delay(500);
+    writePins(ANALOG_PINS, ANALOG_PINS + EMITTER_PINS_COUNT / 2, LOW);
+    writePins(ANALOG_PINS + EMITTER_PINS_COUNT / 2, ANALOG_PINS + EMITTER_PINS_COUNT, HIGH);
+
+    readPins(ANALOG_PINS, ANALOG_PINS + EMITTER_PINS_COUNT / 2, readings_buffer);
+
+
+    delay(500);
+    writePins(ANALOG_PINS + EMITTER_PINS_COUNT / 2, ANALOG_PINS + EMITTER_PINS_COUNT, LOW);
+    delay(500);
+    digitalWrite(READ_READY_EMITTER_PIN, HIGH);
+    delay(500);
+    digitalWrite(READ_READY_EMITTER_PIN, LOW);
+
+    Serial.println("Hardware test ok...");
+}
+
 void setup() {
+  Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(READ_READY_EMITTER_PIN, OUTPUT);
 
+  // Disable write-protect
   R_PMISC->PWPR_b.B0WI = 0;
   R_PMISC->PWPR_b.PFSWE = 1;
 
+  // Configure for analog general-purpose input
   R_PFS->PORT[READ_READY_PORT].PIN[READ_READY_PIN].PmnPFS_b.PMR = 0;
   R_PFS->PORT[READ_READY_PORT].PIN[READ_READY_PIN].PmnPFS_b.PCR = 0;
   R_PFS->PORT[READ_READY_PORT].PIN[READ_READY_PIN].PmnPFS_b.PDR = 0;
   R_PFS->PORT[READ_READY_PORT].PIN[READ_READY_PIN].PmnPFS_b.ASEL = 1;
 
+  // Enable write-protect
   R_PMISC->PWPR_b.PFSWE = 0;
   R_PMISC->PWPR_b.B0WI = 1;
 
   for (int i = 0; i < READ_PINS_COUNT; i++) pinMode(ANALOG_PINS[i], INPUT);
   for (int i = 0; i < EMITTER_PINS_COUNT; i++) pinMode(DIGITAL_PINS[i], OUTPUT);
+
+  hardwareTest();
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);                      // wait for a second
+  delay(100);
 }
