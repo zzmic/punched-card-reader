881d170bf38092f36ed5d4f4bf63ee9dfcc006ea
pkmccann <patrick_mccann@brown.edi>
2025-12-02 15:14:35 -0500

    rename constants with c_UPPERCASE format

--- arduino/punchedCardReader/punchedCardReader.ino
+++ arduino/punchedCardReader/punchedCardReader.ino
@@ -21,8 +21,10 @@
   #include "softwareIntegrationTests.h"
 #endif
 
-const unsigned int TIMER_INT = 31;
-const uint16_t PERIOD_MILLISECONDS = 10;
+const unsigned int c_TIMER_INT = 31;
+const unsigned int c_PERIOD_MILLISECONDS = 10;
+//  period in ms * 1/1000 sec/ms (48 * 1000 * 1000) cycles/sec * (1/1024) scaling factor
+const uint32_t c_COUNTER = (c_PERIOD_MILLISECONDS * (48 * 1000 * 1000 / 1024)) / (1000);
 
 void timerISR() {
   // stop the GPT3 counter
@@ -32,28 +34,24 @@ void timerISR() {
   sendSensorReading(curReading);
 
   #ifdef SOFTWARE_INTEGRATION_TESTING
-  bool complete = checkMessages();
-  if (complete) {
-    // MCU side
-    R_ICU->IELSR_b[TIMER_INT].IR = 0;
-    // Clear the pending interrupt on the CPU side
-    NVIC_ClearPendingIRQ((IRQn_Type) TIMER_INT);
-    Serial.println("\nfinished software integration test");
-    return;
-  }
+    if (checkMessages()) {
+      R_ICU->IELSR_b[c_TIMER_INT].IR = 0;
+      NVIC_ClearPendingIRQ((IRQn_Type) c_TIMER_INT);
+      Serial.println("\nfinished software integration test");
+      return;
+    }
   #endif
 
   // Set up the next noteISR by setting a counter value and turning on GPT3 counter
-  R_GPT3->GTPR = (PERIOD_MILLISECONDS * (48 * 1000 * 1000 / 1024)) / (1000);
-  //  * 1/1000 sec/ms (48 * 1000 * 1000) cycles/sec * (1/1024) scaling factor
-  R_ICU->IELSR[TIMER_INT] = (0x075 << R_ICU_IELSR_IELS_Pos);
+  R_GPT3->GTPR = c_COUNTER;
+  R_ICU->IELSR[c_TIMER_INT] = (0x075 << R_ICU_IELSR_IELS_Pos);
   R_GPT3->GTCR_b.CST = 1;
   
-  // Remember to clear any pending flags!
+  // Clear any pending flags
   // MCU side
-  R_ICU->IELSR_b[TIMER_INT].IR = 0;
+  R_ICU->IELSR_b[c_TIMER_INT].IR = 0;
   // Clear the pending interrupt on the CPU side
-  NVIC_ClearPendingIRQ((IRQn_Type) TIMER_INT);
+  NVIC_ClearPendingIRQ((IRQn_Type) c_TIMER_INT);
 }
 
 void setup() {
@@ -81,13 +79,13 @@ void setup() {
   // Divide the GPT3 clock
   R_GPT3->GTCR = R_GPT3->GTCR & ~(0b111 << 24) | (0b101 << 24); 
   // Disable GPT interrupt on ICU for now
-  R_ICU->IELSR[TIMER_INT] = 0;
+  R_ICU->IELSR[c_TIMER_INT] = 0;
   // Use the Arm CMSIS API to enable CPU interrupts
-  R_ICU->IELSR[TIMER_INT] = (0x075 << R_ICU_IELSR_IELS_Pos); // interrupt enabled on GPT3 overflow 
+  R_ICU->IELSR[c_TIMER_INT] = (0x075 << R_ICU_IELSR_IELS_Pos); // interrupt enabled on GPT3 overflow 
 
-  NVIC_SetVector((IRQn_Type) TIMER_INT, (uint32_t) &timerISR); // set vector entry to our handler
-  NVIC_SetPriority((IRQn_Type) TIMER_INT, 13); // Priority lower than Serial (12)
-  NVIC_EnableIRQ((IRQn_Type) TIMER_INT);
+  NVIC_SetVector((IRQn_Type) c_TIMER_INT, (uint32_t) &timerISR); // set vector entry to our handler
+  NVIC_SetPriority((IRQn_Type) c_TIMER_INT, 13); // Priority lower than Serial (12)
+  NVIC_EnableIRQ((IRQn_Type) c_TIMER_INT);
 
   // kick off first noteISR by setting a counter value and turning on the counter
   R_GPT3->GTPR = 1;
